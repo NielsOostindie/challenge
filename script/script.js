83a9db2d9645e783554ed1d96d3bd9910031732c
NielsOostindie <112725677+NielsOostindie@users.noreply.github.com>
2023-01-09 08:59:42 +0100

    klaar

---
+++ script/script.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var voornaam = document.getElementById("fname").value;
+    var achternaam = document.getElementById("lname").value;
+    alert(voornaam + " " + achternaam);
+}
